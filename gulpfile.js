575fa800d8c85d30ba46acdeb9500841dc7442a6
enigmaturing <javier.glez.martin@gmail.com>
2018-10-31 17:09:01 +0100

    Changes after taking Section 5 Video 13: CSS File architecture (@import, normalize->css reseter, centering elements horizontally and vertically...)

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'), //feature I want to use with postcss in the function of the gulp task 'styles'
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested')
+nested = require('postcss-nested'),
+cssImport = require('postcss-import')
 
 gulp.task('default', function(){
     console.log("Hurra! Has cread una nueva tarea de gulp!")
@@ -20,7 +21,7 @@ gulp.task('styles', function(){
     // we MUST include return here, because src is a asyncronous function
     // and gulp is aware, when the function src is completed.
     return gulp.src('./app/assets/styles/styles.css')
-                .pipe(postcss([cssvars, nested, autoprefixer])) //postcss expcets an array, that's why we include brackets []
+                .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) //postcss expcets an array of features, that's why we include brackets [] ORDERR OF FEATURES MATTERS!!!
                 .pipe(gulp.dest('./app/temp/styles'))
 })
 
